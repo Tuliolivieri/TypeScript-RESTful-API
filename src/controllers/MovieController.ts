94f941496a72c63912585bca2905b534bdf2ea86
Tulio Olivieri <tuliolivieri@gmail.com>
2023-01-24 22:57:54 -0300

    feat: busca, atualização e remoção de filmes

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -9,5 +9,62 @@ export async function createMovie(req: Request, res: Response) {
     return res.status(201).json(movie);
   } catch(error: any) {
     Logger.error(`Erro ao criar filme: ${error.message}`);
+    return res.status(500).json({ message: 'Erro ao cadastrar filme'});
+  }
+}
+
+export async function findMovieById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) return res.status(404).json({ message: 'Filme não encontrado'});
+
+    return res.status(200).json(movie);
+  } catch(error: any) {
+    Logger.error(`Erro ao buscar filme por id: ${error.message}`);
+    return res.status(500).json({ message: 'Erro ao buscar filme'});
+  }
+}
+
+export async function getAllMovies(req:Request, res: Response) {
+  try {
+    const movies = await MovieModel.find();
+    return res.status(200).json(movies);
+  } catch(error: any) {
+    Logger.error(`Erro ao buscar filmes: ${error.message}`);
+    return res.status(500).json({ message: 'Erro ao buscar filmes'});
+  }
+}
+
+export async function deleteById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const movie = await MovieModel.deleteOne({id});
+
+    if (!movie) return res.status(404).json({ message: 'Filme não encontrado' });
+
+    return res.status(200).json({ message: 'Filme deletado com sucesso' });
+  } catch (error: any) {
+    Logger.error(`Erro ao remover filme: ${error.message}`);
+    return res.status(500).json({ message: 'Erro ao remover filme'});
+  }
+}
+
+export async function updateMovie(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    const movie = await MovieModel.findById(id);
+
+    if (!movie) return res.status(404).json({ message: 'Filme não encontrado' });
+
+    await MovieModel.updateOne({ _id: id}, data);
+
+    return res.status(200).json(data);
+  } catch (error: any) {
+    Logger.error(`Erro ao atualizar filme: ${error.message}`);
+    return res.status(500).json({ message: 'Erro ao atualizar filme'});
   }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { createMovie } from './controllers/MovieController';
+import { createMovie, deleteById, findMovieById, getAllMovies, updateMovie } from './controllers/MovieController';
 import { validate } from './middleware/HandleValidation';
 import { movieCreateValidation } from './middleware/MovieValidation';
 
@@ -7,4 +7,9 @@ const router = Router();
 
 export default router.get('/test', (req: Request, res: Response) => {
   res.status(200).send('API Working!');
-}).post('/movie', movieCreateValidation(), validate, createMovie);
+})
+.post('/movie', movieCreateValidation(), validate, createMovie)
+.get('/movie/:id', findMovieById)
+.get('/movie', getAllMovies)
+.delete('/movie/:id', deleteById)
+.patch('/movie/:id', movieCreateValidation(), validate, updateMovie);
